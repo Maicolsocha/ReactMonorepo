cf1adc61fb2dd7d0b7946ee502c98ffeee884fc0
Maicol Socha <maisochajimenez@gmail.com>
2025-08-27 21:42:11 -0500

    star the proyect card X

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './app.css'
+
+export function App(){
+    return (
+        <aside class="card">
+            <div class="usuario">
+                <img src="https://unavatar.io/x/kikobeats" alt="icono usuario" />
+                <div class="usuario-nombre">
+                    <strong>Maicol Fernando Socha Jimenez</strong>
+                    <span>@MaicolSocha</span>
+                </div>
+            </div>
+            <div class="btn-seguir">
+                <button>Seguir</button>
+            </div>
+        </aside>
+    )
+}
