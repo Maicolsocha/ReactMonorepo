36a71d97c3ea4d7bdfd27717ed71050c70936ec9
Maicol Socha <maisochajimenez@gmail.com>
2025-09-10 20:53:06 -0500

    primer uso de estados y pintando el tablero

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react'
 import './App.css'
 
 
@@ -7,10 +8,59 @@ const TURNS = {
    O: 'o'
 }
 
+
+const Square = ({children, isSelected, updateBoard, index}) =>{
+  const className = `square ${isSelected ? 'is-selected': ' '}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
 function App() {
 
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () =>{
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+}
+
   return (
-    <h1>holisss</h1>
+    <main className='board'>
+      <h1>Tres en raya</h1>
+      <section className="game"> 
+        {
+          board.map((_  , index) => {
+            return(
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {index}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+    </main>
   )
 }
 
