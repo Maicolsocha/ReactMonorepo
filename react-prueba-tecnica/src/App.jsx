c6f4dcf31299180e74a4a716b9b53930e713f86b
Maicol Socha <maisochajimenez@gmail.com>
2025-10-09 20:24:18 -0500

    añadiendo estilos y URL imagen

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,3 +1,35 @@
-export function App(){
-    
+import { useEffect, useState } from 'react'
+import './App.css'
+const CAT_ENDPOINT_RAMDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=blue&json=true`
+
+export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RAMDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWord = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWord)
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=blue&json=true`)
+          .then(res => res.json)
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
+  }, [])
+  return (
+    <main>
+      <h1>Hola</h1>
+      <img src={imageUrl} alt={`Image extracteed using the first three words for ${fact}`} />
+      {fact && <p>{fact}</p>}
+    </main>
+  )
 }
--- react-prueba-tecnica/src/main.jsx
+++ react-prueba-tecnica/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import {App} from './src/App.jsx'
+import { App } from './app.jsx'
 
 const root = createRoot(document.getElementById('app'))
 
